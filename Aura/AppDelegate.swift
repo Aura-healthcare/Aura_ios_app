9f6425f96e14718b34f8cb422dd25e6b5e501b3b
Paul <pbancarel@octo.com>
2019-11-25 11:31:42 +0100

    New iOS 13 background task & isolate VivalnkSDK in subproject to avoid warnings from this lib

--- Aura/AppDelegate.swift
+++ Aura/AppDelegate.swift
@@ -1,11 +1,14 @@
 import UIKit
 import Firebase
+import BackgroundTasks
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-
+    private let syncIdentifier = "com.wearablesensor.aura.Aura.sync"
+    private let timeBeforeResync: TimeInterval = 6 * 60 * 60
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         if NSClassFromString("XCTestCase") != nil {
             return true
@@ -19,18 +22,47 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         FirebaseApp.configure()
         
+        registerTasks()
+        
         return true
     }
+    
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        BGTaskScheduler.shared.cancelAllTaskRequests()
+        scheduleSyncWithDevice()
+    }
+}
 
-    func applicationWillResignActive(_ application: UIApplication) {}
-
-    func applicationDidEnterBackground(_ application: UIApplication) {}
-
-    func applicationWillEnterForeground(_ application: UIApplication) {}
-
-    func applicationDidBecomeActive(_ application: UIApplication) {}
-
-    func applicationWillTerminate(_ application: UIApplication) {}
-
+//MARK: Register Background Tasks
+extension AppDelegate {
+    private func registerTasks() {
+        BGTaskScheduler.shared.register(forTaskWithIdentifier: syncIdentifier, using: nil) { task in
+            self.handleSyncWithDeviceTask(task: task as! BGAppRefreshTask)
+        }
+    }
+    
+    private func cancelAllPandingBGTask() {
+        BGTaskScheduler.shared.cancelAllTaskRequests()
+    }
+    
+    private func scheduleSyncWithDevice() {
+        let request = BGAppRefreshTaskRequest(identifier: syncIdentifier)
+        request.earliestBeginDate = Date(timeIntervalSinceNow: timeBeforeResync)
+        
+        do {
+            try BGTaskScheduler.shared.submit(request)
+        } catch {
+            print("Could not schedule app refresh: \(error)")
+        }
+    }
+    
+    private func handleSyncWithDeviceTask(task: BGAppRefreshTask) {
+        //TODO PBA - TODO WORK - Connect to device and get data to store
+        scheduleSyncWithDevice()
+        task.expirationHandler = {
+            // Cancel works if needed
+        }
+        task.setTaskCompleted(success: true)
+    }
 }
 
---
+++ VivaBT/VivaBT/VivaBT.h
@@ -0,0 +1,19 @@
+//
+//  VivaBT.h
+//  VivaBT
+//
+//  Created by Paul Bancarel on 22/11/2019.
+//  Copyright © 2019 Paul Bancarel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VivaBT.
+FOUNDATION_EXPORT double VivaBTVersionNumber;
+
+//! Project version string for VivaBT.
+FOUNDATION_EXPORT const unsigned char VivaBTVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VivaBT/PublicHeader.h>
+
+
